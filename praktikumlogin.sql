0f347c1766f14bbb574535a48ab8210c6879d667
agnespusvitasari18 <ajjaagnessajja@gmail.com>
2026-05-11 18:29:50 +0700

    database

---
+++ praktikumlogin.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE praktikum_login;
+USE praktikum_login;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
